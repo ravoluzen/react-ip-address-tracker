26bee84bd387152fb8d2836c9e300d177f5ad350
Raj Shankar Tiwary <rajshankartiwary@gmail.com>
2022-03-13 03:20:31 +0530

    added fixed height for medium & large screens

--- src/components/DetailsIP.js
+++ src/components/DetailsIP.js
@@ -3,7 +3,7 @@ import React from 'react'
 const DetailsIP = ({ ipAddress, location, timezone, isp}) => {
   return (
     <div 
-    className='bg-white w-4/5 h-auto rounded-xl shadow-lg p-4 mt-32 lg:mt-48 md:mt-48 md:px-10, mt lg:px-8 absolute z-10 flex flex-col justify-between items-center lg:items-start md:items-start lg:flex-row md:flex-row'>
+    className='bg-white w-4/5 h-auto rounded-xl shadow-lg p-4 mt-32 lg:mt-48 md:mt-48 md:px-10, mt lg:px-8 absolute z-10 flex flex-col justify-between items-center lg:items-start md:items-start lg:flex-row md:flex-row md:h-32 lg:h-32'>
         <section className='md:w-1/4 lg:w-1/4'>
             <p className='text-xs text-slate-400 font-medium md:mt-0 md:text-left lg:text-left lg:text-sm'>IP ADDRESS</p>
             <h2 className='text-lg text-black font-medium md:mt-4 lg:mt-4 md:text-left lg:text-left lg:text-2xl'>{ipAddress}</h2>
